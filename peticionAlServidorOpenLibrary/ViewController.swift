f49fff1ee95cc681faa217b1fd4957f24b7e3362
Marcos <marcos.suarez@rocketmail.com>
2015-12-18 09:34:32 +0100

    Se agregó el cuadro de ALERTA

--- peticionAlServidorOpenLibrary/ViewController.swift
+++ peticionAlServidorOpenLibrary/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,18 +37,46 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         // busco los datos de esa WEB.
         let datosURL = sesion.dataTaskWithURL(url!) { (datos, respuestaURL, errores) -> Void in
+            
             // Si hay un error, lo notifico.
             if errores != nil {
                 // No hay conexión a Internet presentar una alerta.
-                print("Se produjo un error en la comunicación: \(errores)")
+                self.mostrarAlerta(errores)
+            } else {
+                let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+                // info del URL.
+                print("info del URL: \(respuestaURL)")
+                // presento los datos recibidos.
+                print("datos recibidos: \(texto!.description)")
+                self.textoRecibido = texto!.description
             }
-            let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
-            
-            // presento los datos recibidos.
-            print("datos recibidos: \(texto!)")
-            self.textoRecibido = texto! as String
         }
         datosURL.resume()
+    }
+    
+    // Alerta No hay Internet.
+    func mostrarAlerta(errorRecibido: NSError?)
+    {
+        // Creo la Alerta.
+        let alerta = UIAlertController(title: "Problemas con la conexión a Internet", message: errorRecibido!.localizedDescription, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        // Defino que hacer cuando se presiona el botón.
+        let accionAlerta = UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel) { (accionArealizar) -> Void in
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        
+        // Agrego al controlador la acción.
+        alerta.addAction(accionAlerta)
+        
+        // presento la alerta.
+        self.presentViewController(alerta, animated: true) { () -> Void in
+            print("Se produjo un error en la comunicación:")
+            print("Codigo: \(errorRecibido!.code)")
+            print("Descripción: \(errorRecibido!.localizedDescription)")
+            print("Razón: \(errorRecibido!.localizedFailureReason)")
+            print("Descripción: \(errorRecibido!.localizedRecoverySuggestion)")
+        }
+        
     }
     
     // Se presionó BUSCAR.
@@ -56,15 +85,15 @@ class ViewController: UIViewController, UITextFieldDelegate {
         if textField == textFieldISBN
         {
             // Quitar el teclado.
-            
+            textFieldISBN.resignFirstResponder()
             // iniciar la busqueda.
             print(" Se introdujo:\(textField.text!)")
             buscarISBN(textField.text!)
         }
-        print("Se presionó enter")
         return true
     }
     
+    // Control de posibles caracteres.
     
 
 }
